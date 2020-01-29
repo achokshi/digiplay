ac233859b8decdde04cf19ad5c8acc669ab0531e
Apoorve Chokshi <apoorve.chokshi@ucalgary.ca>
2020-01-29 06:55:39 -0700

    changes to incorporate QT stories into the middle screen
    Adding text from Dylan's work

--- scripts/middle-screen.js
+++ scripts/middle-screen.js
@@ -23,13 +23,30 @@ function showSlides() {
 
 function showMore(element) {
 	element.innerHTML =
-  	"<p class='ethno'>Ethnocentrism is the tendency to evaluate your group's values and standards as superior to other groups.</p>";
+  	"<p class='ethno'>Ethnocentrism is the tendency to evaluate your group's values and standards as superior to other groups.</p>" +
+    "<p class='qt-stories'> Flocking QT Stories explores how computational simulations of emergent complex behaviours, combined with storytelling, might provide us with ways to deepen our understanding of gender and sexuality-based marginalization and resilience through computer modeling and art.</p>";
   document.getElementById("click-here").remove();
 
   setTimeout(function() {
     resetInfoScreen();
   }, 10000);
 }
+
+/*
+ Flocking QT Stories is an interactive digital art installation that explores how computational simulations of emergent compelx behaviours, combined with storytelling, might provide us with new ways to deepen our understanding of gender and sexuality-based marginalization and resilience through computer modeling and art.
+*/
+
+/*
+The simulation models the emergent behaviour of societal gender and sexuality-based marginalization (harm) and resilience (support), showing how interactions between people and between people and institutions have cumulative effects on an individual person.
+*/
+
+/*
+The stories in the simulation have been collected from people around Alberta with various gender and sexual identities, including cisgender and transgender and queer (LGBTQ+) and heterosexual people.
+*/
+
+/*
+The simulation is part of Dylan Paré's dissertation in Educational Research in the Werklund School of Education.
+*/
 
 
 function resetInfoScreen() {
