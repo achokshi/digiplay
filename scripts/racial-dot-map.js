e148e2fae76899d7a2f8ee1bf98c21ab5fe47b93
Apoorve Chokshi <apoorve.chokshi@ucalgary.ca>
2020-03-03 22:46:38 -0700

    general update
    general updates that I do not want to lose again.

---
+++ scripts/racial-dot-map.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.fade').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        swipe: true,
+        swipeToSlide: true
+    });
+});
